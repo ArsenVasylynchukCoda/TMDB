1879aab4aec98cc54f749c3306b6d2880bfbea9b
oleksandrkrushynskyi <oleksandrkrushynskyi@gmail.com>
2024-01-22 19:46:33 +0200

    lesson 08.01.24

--- src/components/SearchedMovies/SearchedMovies.jsx
+++ src/components/SearchedMovies/SearchedMovies.jsx
@@ -1,24 +1,21 @@
-import React, { useEffect, useRef, useState } from 'react'
+import React, { useEffect } from 'react'
 import { Link } from 'react-router-dom'
 
 const SearchedMovies = ({ searchedMovies, setShowList, value, refInput }) => {
 
-    const ref = useRef()
 
     useEffect(() => {
         if (searchedMovies.length) {
-            console.log('asd')
             setShowList(true)
         }
     }, [value])
 
     useEffect(() => {
         const handleClickOutside = (event) => {
-            if (ref.current && !ref.current.contains(event.target)) {
-            // if (refInput.current.classList.value !== 'searched-movies') {
+            if (refInput.current && !refInput.current.contains(event.target)) {
+                // if (refInput.current.classList.value !== 'searched-movies') {
                 console.log('Закриття вікна')
                 setShowList(false)
-
             }
         }
 
@@ -31,12 +28,14 @@ const SearchedMovies = ({ searchedMovies, setShowList, value, refInput }) => {
     }, [])
 
     return (
-        <div className="searched-movies" ref={ref}>
+        <div className="searched-movies">
             <ul className="searched-movies__list">
                 {searchedMovies.map(elem => {
-                    return <li className="searched-movies__list-item"><Link to="#">&#128269;<img
-                        src={`https://image.tmdb.org/t/p/w500${elem.poster_path}`} alt="movie-banner"/>{elem.title}
-                    </Link></li>
+                    return <li className="searched-movies__list-item">
+                        <Link target="_blank" to="https://www.npmjs.com/package/lodash">&#128269;<img
+                            src={`https://image.tmdb.org/t/p/w500${elem.poster_path}`} alt="movie-banner"/>{elem.title}
+                        </Link>
+                    </li>
                 })}
             </ul>
         </div>
